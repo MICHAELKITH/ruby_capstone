b99cfd2cedaad4d4c433e81de3111518f83ac924
ilyass atlassi <atlassiilyass@gmail.com>
2023-05-30 01:00:24 +0100

    Fix linters errors

--- item.rb
+++ item.rb
@@ -1,25 +1,29 @@
+# frozen_string_literal: true
 
+require 'date'
+
+# create class item
 class Item
-    attr_reader :id , :archived
-    attr_accessor :genre, :author, :source, :label :published_date
-  
-    def initialize(published_date)
-      @id = rand(1..1000)
-      @published_date = published_date
-      @archived = false
-      @genre = genre
-      @author = author
-      @source = source
-      @label = label
-    end
+  attr_reader :id, :archived
+  attr_accessor :genre, :author, :source, :label, :published_date
 
-    def move_to_archive
-        @archived = can_be_archived?
-      end
-    
-      def can_be_archived?
-        Date.today > Date.iso8601(@publish_date).next_year(10)
-      end
-    
-      private :can_be_archived?
+  def initialize(published_date)
+    @id = rand(1..1000)
+    @published_date = published_date
+    @archived = false
+    @genre = genre
+    @author = author
+    @source = source
+    @label = label
+  end
+
+  def move_to_archive
+    @archived = can_be_archived?
+  end
+
+  def can_be_archived?
+    Date.today > Date.iso8601(@publish_date).next_year(10)
+  end
+
+  private :can_be_archived?
 end
