0ee9fb963cd8478b6a48ac4d6f26770556f9fcab
Dev Mike <97337681+MICHAELKITH@users.noreply.github.com>
2023-05-30 15:34:42 +0300

    Solve linters

--- app.rb
+++ app.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # Documentation for the App class
 class App
   def initialize
--- main.rb
+++ main.rb
@@ -1,7 +1,4 @@
-# frozen_string_literal: true
-
 require_relative 'app'
-
 class MainMainu
   @app = App.new
   def user_options(choice)
