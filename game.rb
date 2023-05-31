0e078c882f3551e309e42c951769f8e21d95af82
Issah Mudasir <danladi496@gmail.com>
2023-05-31 18:34:11 +0000

    Initialization-class

--- game.rb
+++ game.rb
@@ -1,5 +1,12 @@
 require 'date'
 
-class Game 
+class Game < item
+    attr_accessor :last_played_at, :name
 
+    def initialize(name, published_date, last_played_at, multiplayer)
+        super(published_date)
+        @last_played_at = last_played_at
+        @multiplayer = multiplayer
+        @name = name
+      end
 end
