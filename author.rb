7305126f8c57288eab4aafab2e06446e57153d09
Issah Mudasir <danladi496@gmail.com>
2023-05-31 18:40:46 +0000

    Added-add-item-method

--- author.rb
+++ author.rb
@@ -10,5 +10,18 @@ class Author
     @last_name = last_name
     @items = []
   end
-  
+
+  def add_item(item)
+    @items << item unless @items.include?(self)
+    item.author = self
+  end
+
+  def to_json(*_args)
+    {
+      'id' => @id,
+      'first_name' => @first_name,
+      'last_name' => @last_name,
+      'items' => @items.map(&:id)
+    }
+  end
 end
