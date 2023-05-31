6a8ce5f4fdd8d0a2c837a03e0902d65fad0c0f26
Issah Mudasir <danladi496@gmail.com>
2023-05-31 18:39:46 +0000

    Added-initializers

--- author.rb
+++ author.rb
@@ -4,5 +4,11 @@ class Author
     attr_accessor :first_name, :last_name
   attr_reader :id, :items
 
+  def initialize(first_name, last_name)
+    @id = Random.rand(1..10_000)
+    @first_name = first_name
+    @last_name = last_name
+    @items = []
+  end
   
 end
