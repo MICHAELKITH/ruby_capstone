d1ee156b689ed4d339e8b7a1896a4763ed61a69b
ilyass atlassi <atlassiilyass@gmail.com>
2023-05-31 20:20:21 +0100

    Add change

--- book.rb
+++ book.rb
@@ -1,18 +1,19 @@
+
 require_relative 'item'
 
 class Book < Item
-  attr_accessor :publisher, :cover_state
-  attr_reader :published_date
+    attr_accessor :publisher, :cover_state
+    attr_reader :published_date
 
-  def initialize(publisher, cover_state, published_date)
-    super(published_date)
-    @publisher = publisher
-    @cover_state = cover_state
-  end
+    def initialize(publisher, cover_state, published_date)
+        super(published_date)
+        @publisher = publisher
+        @cover_state = cover_state
+    end
 
-  private
+    private
 
-  def can_be_archived?
-    super || @cover_state == 'bad'
-  end
+    def can_be_archived?
+        super || @cover_state == 'bad'
+    end
 end
